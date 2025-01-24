82df5a16f2b35992c4a0a295834eef889cc82403
unknown <2020583117@qq.com>
2025-01-24 23:37:30 +0800

    feat: logos组件和problem组件

--- app/(site)/components/logos.tsx
+++ app/(site)/components/logos.tsx
@@ -1,5 +1,62 @@
+import { Marquee } from "@/components/ui/marquee"
+import Image from "next/image"
+
 const Logos = () => {
-  return <section id="logos">logos</section>
+  const logos = [
+    {
+      name: "Vercel",
+      path: "https://res.cloudinary.com/dfhp33ufc/image/upload/v1715881430/vercel_wordmark_dark_mhv8u8.svg"
+    },
+    {
+      name: "Prime",
+      path: "https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/t2awrrfzdvmg1chnzyfr.svg"
+    },
+    {
+      name: "Trustpilot",
+      path: "https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/tkfspxqmjflfllbuqxsi.svg"
+    },
+    {
+      name: "Webflow",
+      path: "https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276560/logos/nymiivu48d5lywhf9rpf.svg"
+    },
+    {
+      name: "Airbnb",
+      path: "https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/pmblusboe7vkw8vxdknx.svg"
+    },
+    {
+      name: "Tina",
+      path: "https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276560/logos/afqhiygywyphuou6xtxc.svg"
+    },
+    {
+      name: "Stackoverflow",
+      path: "https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/ts1j4mkooxqmscgptafa.svg"
+    },
+    {
+      name: "mistral",
+      path: "https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/tyos2ayezryjskox3wzs.svg"
+    }
+  ]
+
+  return (
+    <section id="logos">
+      <div className="container mx-auto px-4 py-12 md:px-8">
+        <h3 className="text-center text-sm font-semibold text-gray-500">TRUSTED BY LEADING TEAMS</h3>
+        <Marquee reverse pauseOnHover className="mt-6 [--duration:20s]">
+          {logos.map(({ name, path }) => (
+            <Image
+              key={name}
+              src={path}
+              alt={name}
+              width={112}
+              height={40}
+              loading="lazy"
+              className="h-10 w-28 opacity-30 grayscale dark:brightness-0 dark:invert"
+            />
+          ))}
+        </Marquee>
+      </div>
+    </section>
+  )
 }
 
 export default Logos
--- app/(site)/components/problem.tsx
+++ app/(site)/components/problem.tsx
@@ -1,5 +1,51 @@
+import { Brain, Zap, Shield } from "lucide-react"
+
 const Problem = () => {
-  return <section id="problem">problem</section>
+  const problemList = [
+    {
+      icon: Brain,
+      title: "Data Overload",
+      desc: "Businesses struggle to make sense of vast amounts of complex data, missing out on valuable insightsthat could drive growth and innovation."
+    },
+    {
+      icon: Zap,
+      title: "Slow Decision-Making",
+      desc: "Traditional data processing methods are too slow, causing businesses to lag behind market changes and miss crucial opportunities."
+    },
+    {
+      icon: Shield,
+      title: "Data Security Concerns",
+      desc: "With increasing cyber threats, businesses worry about the safety of their sensitive information when adopting new technologies."
+    }
+  ]
+
+  return (
+    <section id="problem">
+      <div>
+        <div className="container relative mx-auto max-w-7xl px-4 py-16">
+          <div className="mx-auto space-y-4 pb-6 text-center">
+            <h2 className="font-mono text-sm font-medium uppercase tracking-wider text-primary">Problem</h2>
+            <h3 className="mx-auto mt-4 max-w-xs text-3xl font-semibold sm:max-w-none sm:text-4xl md:text-5xl">
+              Manually entering your data is a hassle.
+            </h3>
+          </div>
+          <div className="mt-12 grid grid-cols-1 gap-8 md:grid-cols-3">
+            {problemList.map(({ icon: Icon, title, desc }) => (
+              <div className="rounded-lg border border-none bg-background text-card-foreground shadow-none" key={title}>
+                <div className="space-y-4 p-6">
+                  <div className="flex h-12 w-12 items-center justify-center rounded-full bg-primary/10">
+                    <Icon className="text-primary" />
+                  </div>
+                  <h3 className="text-xl font-semibold">{title}</h3>
+                  <p className="text-muted-foreground">{desc}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Problem
