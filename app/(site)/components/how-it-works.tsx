43555330f234409b24bbacb3eaf70a8ad678b66e
unknown <2020583117@qq.com>
2025-02-07 22:35:09 +0800

    feat: how-it-works简单开发

--- app/(site)/components/how-it-works.tsx
+++ app/(site)/components/how-it-works.tsx
@@ -1,5 +1,26 @@
 const HowItWorks = () => {
-  return <section id="how-it-works">HowItWorks</section>
+  return (
+    <section id="how-it-works">
+      <div className="container relative mx-auto max-w-7xl px-4 py-16">
+        <div className="mx-auto space-y-4 pb-6 text-center">
+          <h2 className="font-mono text-sm font-medium uppercase tracking-wider text-primary">How it works</h2>
+          <h3 className="mx-auto mt-4 max-w-xs text-3xl font-semibold sm:max-w-none sm:text-4xl md:text-5xl">
+            Just 3 steps to get started
+          </h3>
+        </div>
+        <section id="features">
+          <div className="container">
+            <div className="mx-auto max-w-6xl">
+              <div className="mx-auto my-12 grid h-full items-center gap-10 lg:grid-cols-2">
+                <div>123</div>
+                <div>456</div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </section>
+  )
 }
 
 export default HowItWorks
--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -26,7 +26,7 @@ const BentoCard = ({ title, className, background, description, ...props }: Bent
   <div
     key={title}
     className={cn(
-      "group relative order-3 items-start overflow-hidden rounded-2xl bg-neutral-50 p-6 transition-all duration-500 ease-out hover:bg-blue-500/10 dark:bg-neutral-800 xl:order-none",
+      "group relative order-3 flex flex-col items-start justify-between overflow-hidden rounded-2xl bg-neutral-50 p-6 transition-all duration-500 ease-out hover:bg-blue-500/10 dark:bg-neutral-800 xl:order-none",
       className
     )}
     {...props}
@@ -35,7 +35,7 @@ const BentoCard = ({ title, className, background, description, ...props }: Bent
       <h3 className="mb-2 font-semibold text-primary">{title}</h3>
       <p className="text-foreground">{description}</p>
     </div>
-    <div className="-mb-32 mt-4 flex max-h-64 w-full select-none items-center justify-center px-4 drop-shadow-[0_0_28px_rgba(0,0,0,.1)] transition-all duration-300 group-hover:translate-y-[-10px]">
+    <div className="-mb-20 mt-4 flex max-h-64 w-full select-none items-center justify-center px-4 drop-shadow-[0_0_28px_rgba(0,0,0,.1)] transition-all duration-300 group-hover:translate-y-[-10px]">
       {background}
     </div>
     <div className="pointer-events-none absolute bottom-0 left-0 h-32 w-full bg-gradient-to-t from-neutral-50 dark:from-neutral-900" />
